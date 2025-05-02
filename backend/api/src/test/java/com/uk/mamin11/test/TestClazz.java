8ecd921c53eea1385d780855dba39c99340f4835
Amin <abdimamin@gmail.com>
2025-05-02 19:40:27 +0100

    add sample api endpoint

---
+++ backend/api/src/test/java/com/uk/mamin11/test/TestClazz.java
@@ -0,0 +1,16 @@
+package com.uk.mamin11.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TestClazz {
+
+    @Test
+    void test() {
+        // This is a placeholder test method
+        Assertions.assertTrue(true);
+    }
+}
