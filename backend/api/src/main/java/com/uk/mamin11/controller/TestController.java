4e3d04274251f1b4491fe1e030e27218ffe429bb
Amin <abdimamin@gmail.com>
2025-05-02 19:58:12 +0100

    update test controller request method

--- backend/api/src/main/java/com/uk/mamin11/controller/TestController.java
+++ backend/api/src/main/java/com/uk/mamin11/controller/TestController.java
@@ -1,4 +1,5 @@
 package com.uk.mamin11.controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
